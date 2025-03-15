6ed5254f280c64586b7ceffdd7ef3b567e1edd74
ArtemKohanenko <kartem0255@gmail.com>
2025-03-16 00:37:25 +0600

    build: passport-jwt

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         ExtractJwt.fromAuthHeaderAsBearerToken(),
       ]),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: process.env.JWT_SECRET as string,
     });
   }
 
